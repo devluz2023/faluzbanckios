e74f4e9883dd3e07c0d02982ef8fda4e5157ca07
binhojulix <fabio.jdluz@gmail.com>
2024-03-08 17:15:30 -0300

    Initial Commit

---
+++ faluzbanckios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  faluzbanckios
+//
+//  Created by fabio luz on 08/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ faluzbanckios/faluzbanckiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  faluzbanckiosApp.swift
+//  faluzbanckios
+//
+//  Created by fabio luz on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct faluzbanckiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
